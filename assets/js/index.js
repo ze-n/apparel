c749ac8b5d50a8296a6ce7c644fa7bf0a4d8a479
senku <senku_snap@protonmail.com>
2021-06-17 19:27:00 +0530

    fixing accessibility issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
 const email = form.querySelector("input[type='email']");
-const error = form.querySelector(".email");
+const error = document.querySelector(".email");
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
