4d2fb6a3947e8f0e393be14ad99b3c7616ca272a
senku <senku_snap@protonmail.com>
2021-06-17 17:49:04 +0530

    done

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const email = form.querySelector("input[type='email']");
+const error = form.querySelector(".email");
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (validateEmail(email.value)) {
+        error.classList.remove("error");
+    } else {
+        error.classList.add("error");
+    };
+});
